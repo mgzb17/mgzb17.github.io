ba4ff4184c733ae1956116ba9583345aff5075a4
mgzb17 <bon17012@byui.edu>
2021-02-05 13:57:22 -0700

    update

--- lesson5/preston-5.js
+++ lesson5/preston-5.js
@@ -5,16 +5,30 @@ window.addEventListener('load', ()=>{
     hambutton.addEventListener('click', ()=> {mainnav.classList.toggle('responsive')}, false);
 
     window.onresize = ()=> {if(window.innerWidth>760) mainnav.classList.remove('responsive');}; //when the windows are a certain size, make the responsive off in case it is on.
+// ---------------------------------------------
+    const currentDate = document.querySelector(".date");
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = today.getFullYear();
+    var d = new Date();
+    var weekday = new Array(7);
+    weekday[0] = "Sunday";
+    weekday[1] = "Monday";
+    weekday[2] = "Tuesday";
+    weekday[3] = "Wednesday";
+    weekday[4] = "Thursday";
+    weekday[5] = "Friday";
+    weekday[6] = "Saturday";
+
+    var n = weekday[d.getDay()];
+
+    today = n + ', ' + mm + '/' + dd + '/' + yyyy;
+    currentDate.textContent = today;
+// ---------------------------------------------
+    const cry = document.querySelector("#copyrightyear");
+    cry.textContent = new Date().getFullYear();
+
 });
 
-// function date (today) {
-//     console.log("hi");
-// var today = new Date();
-// var dd = String(today.getDate()).padStart(2, '0');
-// var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-// var yyyy = today.getFullYear();
 
-// today = mm + '/' + dd + '/' + yyyy;
-// document.getElementsByClassName("date")[0].innerHTML =today;
-
-// }
