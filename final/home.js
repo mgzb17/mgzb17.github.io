e5e90382b22c9a51a6c0decb78ed80932c761438
mgzb17 <bon17012@byui.edu>
2021-04-07 10:43:42 -0600

    update

--- final/home.js
+++ final/home.js
@@ -38,47 +38,47 @@ document.getElementById('icon').setAttribute('alt', desc);
 
 }); 
 // ----------------------------------------------
-const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=6f16444b6f9dc85d238029a2d54a5e18&units=imperial";
-fetch(apiURL)
-  .then((response) => response.json())
-  .then((jsObject) => {
-    console.log(jsObject);
-    document.getElementById('current-temp1').textContent = jsObject.main.temp;
+// const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=6f16444b6f9dc85d238029a2d54a5e18&units=imperial";
+// fetch(apiURL)
+//   .then((response) => response.json())
+//   .then((jsObject) => {
+//     console.log(jsObject);
+//     document.getElementById('current-temp1').textContent = jsObject.main.temp;
 
-const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
-const desc = jsObject.weather[0].description;  // note how we reference the weather array
-document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
-document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
-document.getElementById('icon').setAttribute('alt', desc);
+// const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
+// const desc = jsObject.weather[0].description;  // note how we reference the weather array
+// document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
+// document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
+// document.getElementById('icon').setAttribute('alt', desc);
 
-}); 
-// ----------------------------------------------
-const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=6f16444b6f9dc85d238029a2d54a5e18&units=imperial";
-fetch(apiURL)
-  .then((response) => response.json())
-  .then((jsObject) => {
-    console.log(jsObject);
-    document.getElementById('current-temp2').textContent = jsObject.main.temp;
+// }); 
+// // ----------------------------------------------
+// const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=6f16444b6f9dc85d238029a2d54a5e18&units=imperial";
+// fetch(apiURL)
+//   .then((response) => response.json())
+//   .then((jsObject) => {
+//     console.log(jsObject);
+//     document.getElementById('current-temp2').textContent = jsObject.main.temp;
 
-const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
-const desc = jsObject.weather[0].description;  // note how we reference the weather array
-document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
-document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
-document.getElementById('icon').setAttribute('alt', desc);
+// const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
+// const desc = jsObject.weather[0].description;  // note how we reference the weather array
+// document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
+// document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
+// document.getElementById('icon').setAttribute('alt', desc);
 
-}); 
-// ----------------------------------------------
-const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=6f16444b6f9dc85d238029a2d54a5e18&units=imperial";
-fetch(apiURL)
-  .then((response) => response.json())
-  .then((jsObject) => {
-    console.log(jsObject);
-    document.getElementById('current-temp3').textContent = jsObject.main.temp;
+// }); 
+// // ----------------------------------------------
+// const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=6f16444b6f9dc85d238029a2d54a5e18&units=imperial";
+// fetch(apiURL)
+//   .then((response) => response.json())
+//   .then((jsObject) => {
+//     console.log(jsObject);
+//     document.getElementById('current-temp3').textContent = jsObject.main.temp;
 
-const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
-const desc = jsObject.weather[0].description;  // note how we reference the weather array
-document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
-document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
-document.getElementById('icon').setAttribute('alt', desc);
+// const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
+// const desc = jsObject.weather[0].description;  // note how we reference the weather array
+// document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
+// document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
+// document.getElementById('icon').setAttribute('alt', desc);
 
-}); 
+// }); 
