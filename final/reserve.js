f6e6b8c05e4b6f40a112e09e0f6c3e5dd9f72769
mgzb17 <bon17012@byui.edu>
2021-04-07 02:31:53 -0600

    update

--- final/reserve.js
+++ final/reserve.js
@@ -1,26 +1,21 @@
-window.addEventListener('load', () => {
+window.addEventListener('load', ()=>{
     const hambutton = document.querySelector('.ham'); // grabs a class named ham and names it hambutton
     const mainnav = document.querySelector('#navigation');
 
-    hambutton.addEventListener('click', () => {
-        mainnav.classList.toggle('responsive')
-    }, false);
+    hambutton.addEventListener('click', ()=> {mainnav.classList.toggle('responsive')}, false);
 
-    window.onresize = () => {
-        if (window.innerWidth > 760) mainnav.classList.remove('responsive');
-    }; //when the windows are a certain size, make the responsive off in case it is on.
+    window.onresize = ()=> {if(window.innerWidth>760) mainnav.classList.remove('responsive');}; //when the windows are a certain size, make the responsive off in case it is on.
 });
 
 
 // --------------------------------------------- Lazy Loading
 
-window.addEventListener('load', (event) => {
+window.addEventListener('load',(event)=>{
     lazyLoad();
 });
-
 function lazyLoad() {
     const images = document.querySelectorAll("[data-src]");
-    for (const image of images) {
+    for(const image of images){
         image.src = image.getAttribute("data-src");
     }
     //transferred the data of data-src to image
